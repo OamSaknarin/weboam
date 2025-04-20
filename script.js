bfde31c0477c436ac1042d7d16523f49bd0fa183
OamSaknarin <139946356+OamSaknarin@users.noreply.github.com>
2025-04-20 18:09:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+function checkAlreadyLoggedIn() {
+  if (localStorage.getItem("loggedIn") === "true") {
+    window.location.href = "home.html";
+  }
+}
+
+function checkLoginStatus() {
+  if (localStorage.getItem("loggedIn") !== "true") {
+    window.location.href = "index.html";
+  }
+}
+
+function login(event) {
+  event.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username === "Oam" && password === "25433") {
+    localStorage.setItem("loggedIn", "true");
+    window.location.href = "home.html";
+  } else {
+    document.getElementById("popup").style.display = "flex";
+  }
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+function logout() {
+  localStorage.removeItem("loggedIn");
+  window.location.href = "index.html";
+}
